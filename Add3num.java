1d642b474183a75265d47b90435952cfea3ed2be
Ankit Mishra <ankitmishra730751@gmail.com>
2026-08-23 13:58:38 +0530

    Added AAdd3num program

---
+++ Add3num.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Add3num {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number");
+        int ankit = sc.nextInt();
+        System.out.print("Enter second number");
+        int aryan = sc.nextInt();
+        System.out.print("Enter third number");
+        int amit = sc.nextInt();
+
+        System.out.println(ankit+aryan+amit);
+    }
+    
+}
